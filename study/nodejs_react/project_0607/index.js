a0ca51cc0ba50d26fefdf3432754d1a5ea1faa46
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-06-09 16:51:48 +0900

    update study
    0609 nodejs react 강의
    
    8강
    
    mongoDB에 회원가입 요청 처리하기
    nodemon 설치

--- study/nodejs_react/project_0607/index.js
+++ study/nodejs_react/project_0607/index.js
@@ -1,18 +1,47 @@
 const express = require('express')
-
 const app = express()
+const port = 5000
 
-const port = 3000
-
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
 const mongoose =  require('mongoose')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+//application/json
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb+srv://dongjun:dongjun1234@mymongos-wkja4.mongodb.net/<dbname>?retryWrites=true&w=majority',{
     useNewUrlParser : true, useUnifiedTopology :true, useCreateIndex : true, useFindAndModify : false
 }).then(()=> console.log('MongooseDB Connected')).catch(err => console.log(err))
 //
 
 
-app.get('/', (req, res ) => res.send('Hello node Js And Express js'))
+
+
+
+
+app.get('/', (req, res ) => res.send('Hello node Js And Express js 허허'))
+
+//0609 7강
+app.post('/register', (req,res) => {
+    
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 DB에 넣어준다.
+    const user = new User(req.body)  
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({
+            success : true
+        })
+    })
+
+
+})
+
+
 // get('root 디렉토리')
 app.listen(port, ()=> console.log(`Example app listening  on port ${port}`))
 
