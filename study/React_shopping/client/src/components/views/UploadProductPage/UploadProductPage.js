dcdb4457eba26d8921857931b2944006511f64a7
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-06-21 02:15:54 +0900

    React Shopping Project 0621
    update Upload Page
    
    상품 등록 페이지 작성 강의
    Drop-zone
    --- input에 Handler 함수 정의하여 사용 useState
    --- antd 를 활용한 css
    --- map 함수 응용

--- study/React_shopping/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ study/React_shopping/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState} from 'react'
 //rfce 로 functional 생성
+// import './UploadProductPage.css';
+import './UploadProductPage.css';
+import {Typography, Button, Form, Input } from 'antd';
+
+const {TextArea } = Input;
+const Continents = [
+    { key : 1, value : "Africa" },
+    { key : 2, value : "Europe" },
+    { key : 3, value : "Asia" },
+    { key : 4, value : "North America" },
+    { key : 5, value : "South America" },
+    { key : 6, value : "Australia" },
+    { key : 7, value : "Antarctica" }
+    
+]
+
+
 function UploadProductPage() {
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Continent, setContinent] = useState(1)
+    
+    const [Images, setImages] = useState([])
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value);
+    }
+    const descriptionChangeHandler = (event) => {
+        setDescription(event.currentTarget.value);
+    }
+
+    const priceChangeHander = (event) => {
+        setPrice(event.currentTarget.value);
+    }
+
+    const continentChangeHandler = (event) => {
+        setContinent(event.currentTarget.value)
+        // console.log(event.target.value)
+        console.log(Continents[event.currentTarget.value-1]);
+    }
+
     return (
-        <div>
-            UploadProductPage
+        <div className="container-upload">
+            <div>
+                <h2> 여행 상품 업로드</h2>
+            </div>
+            <br/>
+            <br/>
+            <Form>
+                {/* DropZone */}
+            <label>Name</label>
+            <Input onChange={titleChangeHandler} value={Title}/>
+            <br/>
+            <br/>
+            <label>Description</label>
+            <TextArea onChange={descriptionChangeHandler} value = {Description}/>
+            <br/>
+            <br/>
+            <label>Price( $ )</label>
+            <Input type="number" onChange= {priceChangeHander} value = {Price}/>
+            <br/>
+            <br/>
+            <select onChange = {continentChangeHandler} value= {Continent}>
+
+                { Continents.map(item => (
+                <option key = {item.key} value= { item.key }> {item.value}</option>
+                ))}
+
+            </select>
+            <br/>
+            <br/>
+            <br/>
+            <Button>
+                Regist
+            </Button>
+            </Form>
         </div>
     )
 }
