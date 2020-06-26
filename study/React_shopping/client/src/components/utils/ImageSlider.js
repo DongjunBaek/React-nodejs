9e7e86f9ddcaeeeec40c8097e5393d539985dfa7
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-06-27 02:01:47 +0900

    Update LandingPage0627
    update Landing page
    
    card 추가 및 이미지 슬라이드 구현

---
+++ study/React_shopping/client/src/components/utils/ImageSlider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Carousel} from 'antd';
+import './ImageSlider.css';
+
+function ImageSlider(props) {
+    return (
+        <div>
+            <Carousel autoplay >
+
+                {props.images.map((image, index)=> (
+                    <div key={index}>
+                        <img className="carousel-img"
+                            src={`http://localhost:5000/${image}`}/>                        
+                    </div>
+                ))}
+
+            </Carousel>
+        </div>
+    )
+}
+
+export default ImageSlider
--- study/React_shopping/client/src/components/views/LandingPage/LandingPage.js
+++ study/React_shopping/client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,21 @@
-import React, { useEffect} from 'react'
+import React, { useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
 import './LandingPage.css';
-import {Icon, Col, Card, Row} from 'antd';
+import {Icon, Col, Card, Row, Carousel} from 'antd';
 import Meta from 'antd/lib/card/Meta';
-// import ImageSlider from '../../utils/ImageSlider';
+import ImageSlider from '../../utils/ImageSlider';
+
+
 // import Checkbox from './Sections/CheckBox';
 // import Radiobox from './Sections/RadioBox';
 // import SearchFeature from './Sections/SearchFeature';
 // import { continents, price } from './Sections/Datas';
 
 function LandingPage() {
+
+
+    const [Products, setProducts] = useState([])
 
     // loaded componet After
     useEffect(() => {
@@ -21,13 +26,27 @@ function LandingPage() {
         .then(response => {
             if(response.data.success){
                 console.log(response.data);
+                setProducts(response.data.productInfo)
             }else {
                 alert('상품 정보를 가져오는 것에 실패했습니다.')
             }
         })
     }, [])
 
+    const renderCards = Products.map((product, index)=>{
 
+        console.log('product', product)
+        return <Col lg={6} md={8} xs={24} key={index}>
+            <Card                
+                cover={<ImageSlider images={product.images}/>}
+            >
+                <Meta
+                    title = {product.title}
+                    description ={`${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
 
     return (
         <div className="landing-container">
@@ -38,12 +57,12 @@ function LandingPage() {
             {/* Filter*/}
             {/*Search*/}
             {/* Cards*/}
-            <Card
+            {/* gutter 는 간격 */}
+            <Row gutter={16}>
+                {renderCards}
+            </Row>
+                
 
-            >
-
-                <Meta/>
-            </Card>
 
             <div className="landing-article-2">
                 <button>More</button>
