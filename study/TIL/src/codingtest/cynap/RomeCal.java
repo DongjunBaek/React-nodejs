719ba18d88b91edeb13e6720024a24700fdaad38
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-07-07 15:18:49 +0900

    Update RomeCal.java

--- study/TIL/src/codingtest/cynap/RomeCal.java
+++ study/TIL/src/codingtest/cynap/RomeCal.java
@@ -1,35 +1,14 @@
 package codingtest.cynap;
-
 import java.util.Scanner;
-
+/**
+ * 2020.07
+ * 로마자 계산기
+ * @author Dongjun
+ * 문제 출처 : 사이냅소프트 채용퀴즈.
+ */
 public class RomeCal {
 
 	public static void main(String[] args) {
-		
-		/**
-		 * i = 1  
-		 * ii = 2
-		 * iii = 3
-         * iv = 4
-         * v = 5
-         * vi = 6
-         * vii = 7
-         * viii = 8
-         * ix = 9
-         * x = 10
-         * 
-         * xi = 11
-         * xii = 12
-         * xiii = 13
-         * xiv = 14
-         * xv = 15
-         * xvi = 16
-         * xvii = 17
-         * xviii = 18
-         * xix = 19
-         * xx = 20
-		 */
-		
 		
 		RomeCal r = new RomeCal();
 
@@ -55,38 +34,60 @@ public class RomeCal {
 //				"xix",
 //				"xx"				
 //				}; 
-//		System.out.println(s1+" = "+r.convertDex(s1));
+		
+		// TestCase
 		String testcase[] = new String[] {"I + I", "XX - IV","III * VI","X / III","III / X","X - XXX","XX + XX"} ;
 		
 		for(String s : testcase) {
 			System.out.println(s+" = "+r.solution(s));
-//			System.out.println(r.solution(s));
-//			r.solution(s);
 		}
-
+		
+		// User input
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print("수식을 입력하세요(ex : I + I) (종료: n):");
+			String userInput = sc.nextLine().toUpperCase();
+			
+			if(userInput.charAt(0) == 'N') {
+				System.out.println("종료되었습니다.");
+				break;
+			} else if(userInput.split(" ").length != 3){
+				System.out.println("잘못 된 입력입니다.");
+			} else {
+				System.out.println(userInput + "= " + r.solution(userInput));
+			}
+		}
+		
+		
+		
 		
 	}
 	
 	private String solution(String s) {
-		
+		//결과 값 저장할 변수 선언 및 초기화
 		String answer = "";
 		
+		//오류메세지를 저장할 변수 선언 및 초기화
 		String msg = "";
 		
+		//입력받은 식을 저장할 객체 배열 선언 및 초기화
 		String[] sArr = s.split(" ");
 		
-		int num1 = convertDex(sArr[0]);
-		int num2 = convertDex(sArr[2]);
+		//로마 숫자를 변환
+		int num1 = convertDec(sArr[0]);
+		int num2 = convertDec(sArr[2]);
 		char op = sArr[1].charAt(0);
 
 		int result = 0;
 		
+		
+		// 입력값 검사		
 		if(!checkNum(num1) || !checkNum(num2)) {
 			msg= "입력값의 범위를 벗어났습니다.";
 			return msg;
 			
 		} else {
-			
+			//계산
 			switch(op) {
 			case '+':
 				result = (num1 + num2);
@@ -111,11 +112,12 @@ public class RomeCal {
 				}				
 				break;
 			default:
-				msg = "입력된 연산자의 형식이 옳지 않습니다."+op;
-				
+				msg = "입력된 연산자의 형식이 옳지 않습니다."+op;				
 				return msg;
 			}
 		}
+		
+		//결과값 확인
 		
 		if(!checkNum(result) && msg.length() == 0) {
 			return answer = "범위를 벗어났습니다.";
@@ -123,6 +125,7 @@ public class RomeCal {
 			return msg;
 		} 
 		
+		//결과 값을 로마자로 변환.
 		answer = convertRome(result);
 		
 		return answer;
@@ -130,6 +133,7 @@ public class RomeCal {
 
 	
 	
+	//10진수를 로마숫자로 변환하는 메소드
 	private String convertRome(int result) {
 		
 		String answer = "";
@@ -168,8 +172,12 @@ public class RomeCal {
 		
 		return answer.toUpperCase();
 	}
-
-	public int convertDex(String s) {
+	
+	//로마숫자를 10진수로 변환
+	public int convertDec(String s) {
+		// keyword : i의 개수는 0~3개
+		// i가 1개일 때 위치에 따라 +1 or -1 결정
+		// 앞의 수가 뒤의 오는 숫자보다 작을 수 없다.
 		int answer = 0;
 		s = s.toLowerCase();
 				
@@ -195,9 +203,11 @@ public class RomeCal {
 		
 	}
 	
+	//숫자 범위 확인.	
 	public boolean checkNum(int num) {
 		boolean bool = true;
 		if(num > 39 || num < 1) {
+			//범위를 벗어나면 false
 			bool = false;
 		}
 		return bool;
