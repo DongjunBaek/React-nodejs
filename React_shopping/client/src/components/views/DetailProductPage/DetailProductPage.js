171c54cd080aa9fb5d9f0ede6c39f14ab1ac6aa9
DongJunBaek <49776529+DongjunBaek@users.noreply.github.com>
2020-07-10 03:13:28 +0900

    Learn React-shopping-project 0710
    Add dependency :  "react-image-gallery"
    
    Update files :
    DetailProductpage.js
    Index.css
    
    Add files :
    ProductImages.js
    ProductInfo

--- React_shopping/client/src/components/views/DetailProductPage/DetailProductPage.js
+++ React_shopping/client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,17 +1,22 @@
-import React,{useEffect} from 'react'
-
+import React,{useEffect,useState} from 'react'
 import axios from 'axios'
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
+import {Row, Col} from 'antd';
 
 function DetailProductPage(props) {
 
     const productId = props.match.params.productId
+    //object 형태 {}로 초기화
+    const [Product, setProduct] = useState({})
 
     useEffect(() => {
         
         axios.get(`/api/product/product_by_id?id=${productId}&type=single`)
             .then(response => {
                 if(response.data.success){
-                    console.log('response.data',response.data)
+                    // console.log('response.data',response.data)
+                    setProduct(response.data.product[0])
                 } else {
                     alert("Filed Load detail Page")
                 }
@@ -23,9 +28,26 @@ function DetailProductPage(props) {
 
 
     return (
-        <div>
-            DetailProductPage
+        <div style={{ width: '100%', padding: '3rem 4rem' }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <h1>{Product.title}</h1>
         </div>
+
+        <br />
+        <br />
+        {/* gutter => padding */}
+        {/* Row -> 행 Col -> 열  lg-> large::큰화면일때 절반 sm-> small:: 작은화면일때 24(전체)  */}
+        <Row gutter={[16, 16]}>
+            <Col lg={12} sm={24}>
+                {/* {ProductImage} */}
+                <ProductImage detail={Product}/>
+            </Col>
+            <Col lg={12} sm={24}>
+                {/* {ProductInfo} */}
+                <ProductInfo />
+            </Col>
+        </Row>
+    </div>
     )
 }
 
